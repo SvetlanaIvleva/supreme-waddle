e4215a3fff57b460283083f5e4a6e4039900bf1f
SvetlanaIvleva <159472953+SvetlanaIvleva@users.noreply.github.com>
2024-04-30 16:54:01 +0300

    Add files via upload

---
+++ WordFinder1/Avtorizatsiya.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+namespace GDKlaba3WordSearcher
+{
+    public partial class Avtorizatsiya : Form
+    {
+        public Avtorizatsiya()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WordFinder1/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace GDKlaba3WordSearcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
